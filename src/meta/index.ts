33f683ec64d468aae991e1afd7ec9f6b8989bc3e
rudy3091 <grayblack313@gmail.com>
2021-07-17 01:24:36 +0900

    Add meta directory for metadata

---
+++ src/meta/index.ts
@@ -0,0 +1,7 @@
+export const tagsData = [
+  "tag test 1",
+  "tag test 2",
+  "tag test 3",
+  "tag test 4",
+  "looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong tag name",
+];
