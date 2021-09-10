4ded1cb4c7d1f9e4e1943e5757651ef22dfd28b2
rudy3091 <grayblack313@gmail.com>
2021-09-10 22:14:02 +0900

    Cleanup everything
    i will move on to gatsby

--- src/meta/index.ts
+++
@@ -1,7 +0,0 @@
-export const tagsData = [
-  "tag test 1",
-  "tag test 2",
-  "tag test 3",
-  "tag test 4",
-  "looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong tag name",
-];
