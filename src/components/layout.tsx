c50b64f304fdfa52aa850f6565d1b378eeec95d8
rudy3091 <grayblack313@gmail.com>
2021-09-12 23:04:43 +0900

    Style update on layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql, PageProps } from "gatsby";
 
-const Layout = ({ children }: PageProps) => {
+const Layout: React.FC<PageProps> = ({ children }: PageProps) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -17,6 +17,15 @@ const Layout = ({ children }: PageProps) => {
   return (
     <>
       <Helmet title={data.site.siteMetadata.title} />
+      <style>
+        {`
+          html,
+          body {
+          margin: 0;
+          padding: 0;
+          }
+        `}
+      </style>
       <div
         style={{
           margin: `0 auto`,
