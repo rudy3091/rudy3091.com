fab0c87c968f91a6dbb41e8c5d9db1553c767a92
rudy3091 <grayblack313@gmail.com>
2022-01-27 01:11:30 +0900

    Add theme data file

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import { Helmet } from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
+import { ThemeProvider } from "styled-components";
 import Logo from "../atoms/Logo";
 import S from "./style";
 import GlobalStyle from "../../styles/global";
+import theme from "../../styles/theme";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -24,17 +26,21 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <>
       <Helmet title={data.site.siteMetadata.title} />
       <GlobalStyle />
-      <S.Container>
-        <header>
-          <S.Nav>
-            <Logo routeHome></Logo>
-          </S.Nav>
-        </header>
-        <main style={{ paddingTop: `50px`, maxWidth: `800px`, width: `100%` }}>
-          {children}
-        </main>
-        <footer></footer>
-      </S.Container>
+      <ThemeProvider theme={theme}>
+        <S.Container>
+          <header>
+            <S.Nav>
+              <Logo routeHome></Logo>
+            </S.Nav>
+          </header>
+          <main
+            style={{ paddingTop: `50px`, maxWidth: `800px`, width: `100%` }}
+          >
+            {children}
+          </main>
+          <footer></footer>
+        </S.Container>
+      </ThemeProvider>
     </>
   );
 };
---
+++ src/styles/theme.ts
@@ -0,0 +1,5 @@
+const colors = {
+  WHITE: '#f8f8f8',
+};
+
+export default { colors };
